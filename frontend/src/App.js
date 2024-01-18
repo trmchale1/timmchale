d800ecdaa136a07f9692d0accb04710bbd903e33
trmchale1 <trmchale@gmail.com>
2024-01-17 18:08:45 -0700

    updating frontend, making it simpler

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,11 @@ import React from 'react';
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-        tim: "Tim!"
-    };
-  }
-
-  componentDidMount() {
-      fetch('/api/')
-          .then(response => response.json())
-          .then(response => this.setState({'tim': response}))
-  }
-
   render() {
-    return <h1>Hello {this.state.tim}</h1>;
+    return ( 
+    <h1>Hello World!</h1>
+    );
+
   }
 
 }
