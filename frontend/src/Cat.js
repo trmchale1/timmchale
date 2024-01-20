a26fd278e03a3eb22df0e34f6b9725ee37e1923a
trmchale1 <trmchale@gmail.com>
2024-01-20 14:39:19 -0700

    was able to add mutiple vertical divs to frontend

---
+++ frontend/src/Cat.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Cat({name}) {
+  return (
+    <h1>The cat's name is {name}</h1>
+    );
+}
+
+export default Cat;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Cat from './Cat';
+import Image from './Image';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+    <Cat name="Jungle" />
+    <Image />
+  </React.StrictMode>
+,
   document.getElementById('root')
 );
