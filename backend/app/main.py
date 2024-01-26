327cf21f606dada9318dec1fdab03f8e57cf0ebb
trmchale1 <trmchale@gmail.com>
2024-01-26 16:09:00 -0700

    deleted fastapi

--- backend/app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-app = FastAPI()
-
-
-@app.get("/", tags=["root"])
-async def read_root() -> dict:
-    return {"message": "This is a message from the backend."}
